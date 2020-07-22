85da9a0116c76260ab52f0d45af8f98eb27f325d
deepak1patel <deepakpatelg16@gmail.com>
2020-07-22 18:27:29 +0530

    ending v.1

--- README.md
+++ README.md
@@ -9,7 +9,11 @@ This is the famliy app just used by the our family not by others.
 UI
 1. New split list to divide events into upcomming events and the events that will happen in next year.
 2. New sliver app bar and list to add nearest event in there. Count down will be there where now of days will be displayed until the event.
-3. New menu bar and refresh logo to update the data.
+3. Added pull down to refresh.
 
 Code 
 1. Created new utility file to add all the reuseable functions.
+2. Added online data to update data on fly.
+3. Added read from json file both from online and in local.
+4. Added sorted fun such that events will be in order.
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 
-import 'package:notification/Generate.dart';
+import 'Generate.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: Colors.black,
+      themeMode: ThemeMode.light,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Phoenix"),
+          leading: Icon(Icons.menu),
+        ),
         body: GenerateList(),
+        floatingActionButton: ClipOval(
+          child: Material(
+            color: Colors.blue,
+            child: SizedBox(
+              child: Center(
+                child: IconButton(
+                  icon: Icon(
+                    Icons.whatshot,
+                    color: Colors.red,
+                  ),
+                  onPressed: () {},
+                  splashColor: Colors.red,
+                ),
+              ),
+              width: 50.0,
+              height: 50.0,
+            ),
+          ),
+        ),
       ),
     );
   }
